d0b920f9912763a1422bd44b8be56e41c2eb2ff6
Max <savagecm@users.noreply.github.com>
2018-02-02 13:36:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#work_path=$(dirname $0)
+work_path=$(dirname $(readlink -f $0))
+echo 'base working dir is :' $work_path
+git clone --recursive https://github.com/Cylix/cpp_redis.git&&cd cpp_redis&&mkdir -p build&&cd build&&make&&make install
 
-mkdir build&&cd build&&cmake ..&&make&&make install
-
+mkdir -p build&&cd build&&cmake ..&&make&&make install
