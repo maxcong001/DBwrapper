78a77cb3d93ac30788eaddb1ae8c91d88f9657d5
Max <savagecm@users.noreply.github.com>
2018-02-02 14:02:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 #work_path=$(dirname $0)
 work_path=$(dirname $(readlink -f $0))
 echo 'base working dir is :' $work_path
-git clone --recursive https://github.com/Cylix/cpp_redis.git&&cd cpp_redis&&git submodule update --init --recursive&&mkdir -p build&&cd build&&make&&make install
+cd $work_path
+git clone https://github.com/Cylix/tacopie.git&&cd tacopie&&mkdir -p build&&cd build&&make&&make install
+cd $work_path
+git clone --recursive https://github.com/Cylix/cpp_redis.git&&cd cpp_redis&&mkdir -p build&&cd build&&make&&make install
 
 cd $work_path&&mkdir -p build&&cd build&&cmake ..&&make&&make install
